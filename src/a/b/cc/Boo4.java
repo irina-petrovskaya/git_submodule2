61a7bdf51302f36c0b18c6ff0afc72d3b5fc4f61
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-06-08 14:10:07 +0300

    submodule1: Foo1.java, Foo2.java submodule2: Boo4.java git_submodule1, git_submodule2

--- src/a/b/cc/Boo4.java
+++ src/a/b/cc/Boo4.java
@@ -4,6 +4,7 @@ package a.b.cc;
  * *******************************
  * Created by Irina.Petrovskaya on 3/2/2017.
  * Project: git_submodules_test1
+ * submodule2
  * *******************************
  */
 public class Boo4 {
